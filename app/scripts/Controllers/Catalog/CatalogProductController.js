34316b114356fcf45ef99490f6c60ca133848623
Florian Sydekum <florian@sydekum.de>
2014-03-19 21:35:05 +0100

    Fixed url reload bug

--- app/scripts/Controllers/Catalog/CatalogProductController.js
+++ app/scripts/Controllers/Catalog/CatalogProductController.js
@@ -1,8 +1,10 @@
 Webster.CatalogProductController = Ember.ObjectController.extend({
     product: function(){
         var products = Webster.Session.get('productCollection');
-        var product =  products.objectAt(0);
-        console.log('jo product oida: ' + product);
-        return product;
+        if(products){
+            var product =  products.objectAt(0);
+            console.log('jo product oida: ' + product);
+            return product;
+        }
     }.property('Webster.Session.productCollection')
 });
